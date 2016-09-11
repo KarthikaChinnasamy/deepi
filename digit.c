4b0da76c5f4a66a23759670de8e536a4011060cd
KarthikaChinnasamy <karthikakcs21@gmail.com>
2016-09-11 16:55:11 +0530

    Update digit.c

--- digit.c
+++ digit.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
-void main()
-{
+#include <stdio.h>
+
+int main(void) {
+
+
 int i,j;
 int count=0;
 scanf("%d",&j);
-while(n!=0)
+while(j!=0)
 {
 i=j%10;
 j=j/10;
 count=count+1;
 }
 printf("%d",count);
+	// your code goes here
+	return 0;
 }
